8696c6170c80bbbe0750bbfe767e1b96c4f002ab
Presley Graham <jpresleygraham@gmail.com>
2025-02-20 14:06:51 -0500

    fix diagnostic

--- rplugin/python3/structs/diagnostic.py
+++ rplugin/python3/structs/diagnostic.py
@@ -28,13 +28,16 @@ class Diagnostic:
 
     @classmethod
     def from_nvim_diagnostic(cls, nvim_diagnostic: dict[str, Any]) -> Diagnostic:
-        fields = {
-            field.name: field.type for field in Diagnostic.__dataclass_fields__.values()
-        }
-        kwargs = {}
-        for field_name, field_type in fields.items():
-            value = nvim_diagnostic[field_name]
-            if field_type == DiagnosticSeverity:
-                value = DiagnosticSeverity(value)
-            kwargs[field_name] = value
-        return Diagnostic(**kwargs)
+        return cls(
+            bufnr=nvim_diagnostic["bufnr"],
+            lnum=nvim_diagnostic["lnum"],
+            end_lnum=nvim_diagnostic["end_lnum"],
+            col=nvim_diagnostic["col"],
+            end_col=nvim_diagnostic["end_col"],
+            severity=DiagnosticSeverity(nvim_diagnostic["severity"]),
+            message=nvim_diagnostic["message"],
+            source=nvim_diagnostic["source"],
+            code=nvim_diagnostic.get("code"),
+            user_data=nvim_diagnostic.get("user_data"),
+            namespace=nvim_diagnostic.get("namespace"),
+        )
